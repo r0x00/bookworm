9bf8e93952ef39459ab0e43f355e63b3638a3ae1
alexia <alexia.zolesi@convergint.com>
2024-10-17 06:36:38 -0300

    :green_heart: adding translation route

--- api/services/Dictionary.js
+++ api/services/Dictionary.js
@@ -1,11 +1,36 @@
-const db = require('../../db/mysql');
+const axios = require('axios');
 
 class Dictionary {
-    static translation (req, res, next) {
-        console.log(db.query({ sql: '', query: ''}));
-        res.send('hihi')
+    static async translation (req, res, next) {
+        try {
+            const { query, languageFrom, languageTo } = req.query;
+
+            if(!query) return res.status(400).send('Please add query');
         
-    }
+            if(!languageFrom) return res.status(400).send('Please add languageFrom');
+
+            if(!languageTo) return res.status(400).send('Please add languageTo');
+
+            const translation = await axios({
+                method: 'get',
+                url: `https://api.mymemory.translated.net/get?q=${query}&langpair=${languageFrom}|${languageTo}`
+            });
+
+            const result = {
+                found: false,
+            };
+
+            if(translation.data?.responseData?.match) {
+                result.found = true;
+                result.translation = translation.data?.responseData?.translatedText
+            };
+
+            res.status(200).send(result);
+
+        } catch(_error) {
+            res.status(500).send(_error);
+        };
+    };
 }; 
 
 module.exports = Dictionary;
