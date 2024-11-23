6f701dc65c8e1d5e461205bbc491433e0824d1ab
alexia <alexia.zolesi@convergint.com>
2024-11-23 09:04:26 -0300

    upgrade development script

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,22 +1,30 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('#i@UP6dS8axcYtSTa##Z@1R@VUPXExsJhv_', 'bookworm', '#pmDP#Wi@7JtXEhn@GT#_7mrOG#ItFk_#o', {
-  host: '0.0.0.0', 
-  port: 3306,
-  dialect: 'mysql', 
-  logging: console.log, 
-  // force: true
-});
+class SequelizeORM {
+  static sequelize;
 
-const syncDatabase = async () => {
-  try {
-      await sequelize.sync({ force: false });
+  constructor() {
+    this.sequelize = new Sequelize('#i@UP6dS8axcYtSTa##Z@1R@VUPXExsJhv_', 'bookworm', '#pmDP#Wi@7JtXEhn@GT#_7mrOG#ItFk_#o', {
+      host: '0.0.0.0', 
+      port: 3306,
+      dialect: 'mysql', 
+      // logging: console.log, 
+    });
+  };
+
+  static async syncDatabase() {
+    try {
+      await this.sequelize.sync({ force: false });
+
       console.log('Database & tables created!');
-  } catch (error) {
+
+    } catch (error) {
       console.error('Error syncing database:', error);
-  }
+    };
+  };
 };
 
-syncDatabase();
 
-module.exports = sequelize;
+const sequelize = new SequelizeORM();
+
+module.exports = sequelize.sequelize;
--- scripts/development.sh
+++ scripts/development.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-docker-compose -f docker-registry/docker-compose.yml up -d &&
+docker-compose -f docker-registry/docker-compose.yml up -d
+
+function checkDBContainer {
+
+    while ! docker exec db mysqladmin --user="bookworm" --password="#pmDP#Wi@7JtXEhn@GT#_7mrOG#ItFk_#o" --host "127.0.0.1" ping --silent &> /dev/null ; do
+        echo "Waiting for database connection..."
+
+        sleep 2
+    done
+}
+
+checkDBContainer
 
 nodemon ./bin/www
